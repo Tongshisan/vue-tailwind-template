660b5b5119e39768f2f75b1fa7795270dd1b8791
李智 <lizhi01@jwzg.com>
2025-02-21 16:42:30 +0800

    feat: 更新打字机

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@ import { ref } from "vue";
 import Typewriter from "./components/typewriter/index.vue";
 
 const count = ref(0);
+
+const callback = () => {
+  console.log("callback");
+};
 </script>
 
 <template>
   <button @click="count++">Count is: {{ count }}</button>
-  <Typewriter content="Hello, World!" />
+  <Typewriter content="Hello, World!" :callback="callback" />
 </template>
--- src/components/Typewriter/index.vue
+++ src/components/Typewriter/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 
 // 定义 props，添加默认值
 const props = defineProps({
@@ -17,30 +17,33 @@ const props = defineProps({
 // 定义一个 ref 来存储当前显示的文本
 const displayedText = ref("");
 
+const intervalId = ref(null);
+
 // 在组件挂载时开始打字机效果
 onMounted(() => {
   let index = 0;
-  let lastTime = 0;
 
-  const typeWriter = (currentTime) => {
-    if (!props.content) return; // 检查 content 是否为空
+  intervalId.value = setInterval(() => {
+    if (!props.content) {
+      clearInterval(intervalId.value);
+      return; // 检查 content 是否为空
+    }
 
-    if (currentTime - lastTime >= props.time) {
-      if (index < props.content.length) {
-        displayedText.value += props.content[index];
-        index++;
-        lastTime = currentTime;
-      } else {
-        if (props.callback) {
-          props.callback();
-        }
-        return; // 结束动画
+    if (index < props.content.length) {
+      displayedText.value += props.content[index];
+      index++;
+    } else {
+      clearInterval(intervalId.value);
+      if (props.callback) {
+        props.callback();
       }
     }
-    requestAnimationFrame(typeWriter);
-  };
+  }, props.time);
 
-  requestAnimationFrame(typeWriter);
+  // 在组件卸载时清除定时器
+  onUnmounted(() => {
+    clearInterval(intervalId.value);
+  });
 });
 </script>
 
